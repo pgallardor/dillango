0ca8a13ed24a0cd344dcae6f3c042b3a6a58adba
pgallardor <pedrogallardorobinson@gmail.com>
2019-01-09 13:37:16 -0300

    img receive?

--- tests/views.py
+++ tests/views.py
@@ -21,5 +21,5 @@ def json_list(request):
 def receive_file(request):
     img64 = request.POST.get('media')
     img = open("sanik_arrived.png", "wb")
-    img.write(img64.decode('base64'))
+    img.write(img64)
     HttpResponse("Ayy lmao")
